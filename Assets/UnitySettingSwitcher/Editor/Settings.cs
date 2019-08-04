f093ef99ac3e28fa356151e418206aef7008c64b
Yuichi Ogasawara <yuichi.ogasawara.1121@gmail.com>
2019-08-05 06:36:12 +0900

    use-MiniJSON

--- Assets/UnitySettingSwitcher/Editor/Settings.cs
+++ Assets/UnitySettingSwitcher/Editor/Settings.cs
@@ -4,27 +4,25 @@ using System.Collections.Generic;
 namespace uisawara
 {
 
-    [Serializable]
     public class Settings
     {
-        public List<Environment> build_settings = new List<Environment>();
+        public List<Environment> settings = new List<Environment>();
 
-        [Serializable]
         public class Environment
         {
             public string name;
             public string inherit;
             public List<string> scene_list;
-            public KeyValueDictionary build_settings = new KeyValueDictionary();
-            public KeyValueDictionary player_settings = new KeyValueDictionary();
-            public KeyValueDictionary xr_settings = new KeyValueDictionary();
+            public Dictionary<string, object> build_settings = new Dictionary<string, object>();
+            public Dictionary<string, object> player_settings = new Dictionary<string, object>();
+            public Dictionary<string, object> xr_settings = new Dictionary<string, object>();
             public string scripting_define_symbols;
         }
 
         public int GetEnvironmentIndex(string environmentName)
         {
             int index = 0;
-            foreach (var env in build_settings)
+            foreach (var env in settings)
             {
                 if (env.name == environmentName)
                 {
@@ -42,7 +40,7 @@ namespace uisawara
             {
                 return null;
             }
-            return build_settings[index];
+            return settings[index];
         }
 
         public string[] ResolveInherit(string environmentName)
@@ -64,17 +62,38 @@ namespace uisawara
             return result.ToArray();
         }
 
-        public static Settings.Environment Merge(Settings.Environment lhs, Settings.Environment rhs)
+        public static Environment Merge(Settings.Environment lhs, Settings.Environment rhs)
         {
             var result = SettingsUtil.Copy(lhs);
 
-            if (rhs.build_settings != null) result.build_settings.Hang(rhs.build_settings);
-            if (rhs.player_settings != null) result.player_settings.Hang(rhs.player_settings);
-            if (rhs.xr_settings != null) result.xr_settings.Hang(rhs.xr_settings);
+            // Merge settings
+            if (rhs.build_settings != null) Settings.Merge(result.build_settings, rhs.build_settings);
+            if (rhs.player_settings != null) Settings.Merge(result.player_settings, rhs.player_settings);
+            if (rhs.xr_settings != null) Settings.Merge(result.xr_settings, rhs.xr_settings);
+
+            // Special merge settings
             if (!string.IsNullOrEmpty(rhs.scripting_define_symbols)) result.scripting_define_symbols = result.scripting_define_symbols + ";" + rhs.scripting_define_symbols;
             if (rhs.scene_list != null) result.scene_list.AddRange(rhs.scene_list);
 
             return result;
+        }
+
+        /// <summary>
+        /// Merge rhs settings to lhs settings
+        /// </summary>
+        /// <param name="lhs">Lhs.</param>
+        /// <param name="rhs">Rhs.</param>
+        public static void Merge(Dictionary<string, object> lhs, Dictionary<string, object> rhs)
+        {
+            foreach (var kv in rhs)
+            {
+                if (lhs.ContainsKey(kv.Key))
+                {
+                    continue;
+                }
+
+                lhs[kv.Key] = kv.Value;
+            }
         }
 
     }
